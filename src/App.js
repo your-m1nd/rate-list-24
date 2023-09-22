f7c218851a1b5f4d223a1ac6548db39b0cfe6c04
your-m1nd <vi.neurogirl@gmail.com>
2023-09-22 11:28:05 +0300

    add click on element

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
 import './App.css';
 import Rate from './components/Rate';
 import './components/Rate.css';
-
-const rateList = [
-  {
-    name: "Безлимитный 300",
-    url: "/assets/300.JPG",
-    speed: "до 10 Мбит/сек",
-    traffic: "Объём включённого трафика не ограничен",
-  },
-
-  { 
-    name: "Безлимитный 450",
-    url: "/assets/450.JPG",
-    speed: "до 50 Мбит/сек",
-    traffic: "Объём включённого трафика не ограничен",
-  },
-
-  {
-    name: "Безлимитный 550",
-    url: "/assets/550.JPG",
-    speed: "до 100 Мбит/сек",
-    traffic: "Объём включённого трафика не ограничен",
-    isSelected: true,
-  },
-
-  {
-    name: "Безлимитный 1000",
-    url: "/assets/1000.JPG",
-    speed: "до 200 Мбит/сек",
-    traffic: "Объём включённого трафика не ограничен",
-  },
-
-]
+import { useState } from 'react';
 
 function App() {
+
+  const rateList = [
+    {
+      id: 1,
+      name: "Безлимитный 300",
+      url: "/assets/300.JPG",
+      speed: "до 10 Мбит/сек",
+      traffic: "Объём включённого трафика не ограничен",
+    },
+  
+    { 
+      id: 2,
+      name: "Безлимитный 450",
+      url: "/assets/450.JPG",
+      speed: "до 50 Мбит/сек",
+      traffic: "Объём включённого трафика не ограничен",
+    },
+  
+    {
+      id: 3,
+      name: "Безлимитный 550",
+      url: "/assets/550.JPG",
+      speed: "до 100 Мбит/сек",
+      traffic: "Объём включённого трафика не ограничен",
+      isSelected: true,
+    },
+  
+    {
+      id: 4,
+      name: "Безлимитный 1000",
+      url: "/assets/1000.JPG",
+      speed: "до 200 Мбит/сек",
+      traffic: "Объём включённого трафика не ограничен",
+    },
+  
+  ];
+
+  const [selectedId, setSelectedId] = useState(null);
+  const onClick = (id) => {
+    setSelectedId(id);
+  }
+
   return (
     <div className="App">
       <div className="container">
    {
     rateList.map((rate) =>
-    <Rate 
-    name={rate.name} url={rate.url} speed={rate.speed} traffic={rate.traffic} isSelected={rate.isSelected} />
+    <Rate key={rate.id} id={rate.id} name={rate.name} url={rate.url} speed={rate.speed} traffic={rate.traffic} onClick={onClick} selectedId={selectedId}/>
     ) 
   }
   </div>
--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -1,41 +1,21 @@
-import React from "react";
 import '../components/Rate.css';
 
-class Rate extends React.Component {
-    static defaultProps = {
-        name: 'Тариф'
-    }
+const Rate = (props)  => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            pressed: false,
-            pressedStyle: '',
-        };
-    };
-
-    handleChange = () => {
-        this.setState(
-            {pressed: !this.state.pressed,
-            pressedStyle: 'orange',
-            }
-        );
-
-    }
-
-    render() {
-        const {name, url, speed, traffic} = this.props;
-        const style = {backgroundColor: this.state.pressedStyle};
+        const {id, name, url, speed, traffic, selectedId, onClick} = props;
+        
         return (
-        <section className={"rate"} onClick={this.handleChange} style={style}>
+            //добавили по клику изменение тарифа (пока только текст)
+        <section className={"rate"} id={id} onClick={()=>{onClick(id)}} selectedId={selectedId ? 'Тариф выбран' : 'Тариф не выбран'}>
         <h2 className="rate-name">{name}</h2>
         <img className="rate-price" src={url} alt={name}></img>
         <p className="rate-speed">{speed}</p>
         <p className="rate-traffic">{traffic}</p>
-        <p className="rate-choose">{this.state.pressed ? "Тариф выбран" : "Тариф не выбран"}</p>
+        <p className="rate-choose">{selectedId === id ? 'Тариф выбран' : 'Тариф не выбран'}</p>
         </section>
         );
-    }
-}
+        };
 
 export default Rate;
+
+//{this.state.pressed ? "Тариф выбран" : "Тариф не выбран"}
